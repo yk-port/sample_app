0dbc85a97d099b344d901e360a03141915391278
yokokura <yokokura0413@gmail.com>
2020-07-27 12:07:30 +0900

    finish #14

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,16 @@ class User < ApplicationRecord
 
   # association
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships,  class_name: 'Relationship',
+                                  foreign_key: 'follower_id', 
+                                    dependent: :destroy
+  has_many :passive_relationships, class_name: 'Relationship',
+                                  foreign_key: 'followed_id',
+                                    dependent: :destroy
+  has_many :following,                through: :active_relationships,
+                                       source: :followed
+  has_many :followers,                through: :passive_relationships,
+                                       source: :follower
 
   validates :name, presence: true,
                    length: { maximum: 50 }
@@ -84,10 +94,27 @@ class User < ApplicationRecord
     self.reset_sent_at < 2.hours.ago
   end
 
-  # 試作feedの定義
+  # ユーザーのステータスフィードを返す
   def feed
-    Micropost.where("user_id = ?", self.id)
+    following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: self.id)
   end
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    following << other_user
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
 
   private
 
